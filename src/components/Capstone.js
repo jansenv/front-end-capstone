5fd9cad010ae650259b6e447482d337b376f6857
Jansen <jansenv44@gmail.com>
2020-02-04 19:54:13 -0600

    Rendered dummy post with title and author

---
+++ src/components/Capstone.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Post from "./post/Post"
+import "./Capstone.css"
+
+export default () => (
+    <>
+        <h2>CodeSnips</h2>
+
+        <h2>Dashboard</h2>
+        <article className="posts">
+            <Post />
+            <Post />
+            <Post />
+        </article>
+    </>
+)
---
+++ src/components/post/Post.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+    <section className="post">
+        <h3 className="post__title">Title of Post</h3>
+        <div className="post__author">choortlehort</div>
+    </section>
+)
