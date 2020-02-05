563ef3d0bcd9779e31d73c5ee51eb20dc4c4e89b
Jansen <jansenv44@gmail.com>
2020-02-05 11:52:35 -0600

    fixed image pathways

--- src/components/topic-css/CSSPosts.js
+++ src/components/topic-css/CSSPosts.js
@@ -3,7 +3,7 @@ import "./CSSPosts.css"
 
 export default ({ post }) => (
     <section className="post">
-        <img src={post.img}/>
+        <img src={require(`../../images/${post.img}`)}/>
         <h3 className="post__title">{post.title}</h3>
         <div className="post__author">Submitted by user {post.userId}</div>
     </section>
