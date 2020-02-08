fee8bef2c914a0b3dc1e1ff035d9c9ec730d3d6d
Jansen <jansenv44@gmail.com>
2020-02-08 16:22:00 -0600

    removing onclick from button. needs different approach

--- src/components/topic/Topic.js
+++ src/components/topic/Topic.js
@@ -5,10 +5,8 @@ import PostList from "../post/PostList"
 
 export default ({ topic }) => {
 
-    debugger
-
     function RenderTopics() {
-        return <button onClick={() => <PostList key={topic.id} topic={topic} />} id={`${topic.id}`}>
+        return <button id={`${topic.id}`}>
             {topic.name}
         </button>
     }
