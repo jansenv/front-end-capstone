b7203708e5ecad01c663489e78b38df368f2b686
Jansen <jansenv44@gmail.com>
2020-02-10 14:06:10 -0600

    done refactoring

--- src/components/listContainer/ListContainer.js
+++ src/components/listContainer/ListContainer.js
@@ -4,10 +4,11 @@ import { TopicContext } from "../topic/TopicProvider"
 import Post from "../post/Post"
 import { PostContext } from "../post/PostProvider"
 import "./ListContainer.css"
+import { Link } from "react-router-dom"
 
 
 export default (props) => {
-  const { posts } = useContext(PostContext)
+  const { posts, deletePost } = useContext(PostContext)
   const { topics } = useContext(TopicContext)
   const { users } = useContext(UserContext)
 
@@ -24,14 +25,6 @@ export default (props) => {
   }
 
   const sortedPosts = sortArrayByMostRecent(posts)
-
-  function filterPostsByTopicId(topicNum) {
-
-    const FilteredPosts = posts.filter(post =>
-      post.topicId === topicNum)
-
-    return FilteredPosts
-  }
 
   return (
     <>
@@ -57,7 +50,41 @@ export default (props) => {
           sortedPosts.map(post => {
             const foundedUser = users.find(u => u.id === post.userId) || {}
 
-            return <Post key={post.id} post={post} user={foundedUser} {...props} />
+            function LoggedInUserButtons() {
+              if (post.userId === parseInt(localStorage.getItem("activeUser"))) {
+                  return (
+                      <>
+                      <button onClick={() => {
+                          props.history.push(`/edit/${post.id}`)
+                      }}>Edit</button>
+      
+                      <button onClick={() => {
+                          deletePost(post)
+                              .then(() => {
+                                  props.history.push("/")
+                              })
+                      }}>Delete</button>
+                      </>
+                  )
+              }
+          }
+
+          function RenderPosts() {
+            return <section className="post">
+                <div>
+                    {post.votes}
+                </div>
+                <img src={require(`../../images/${post.img}`)} />
+                <h3 className="post__title">
+                    <Link to={`/${post.id}`}>
+                        {post.title}
+                    </Link>
+                </h3>
+                <div className="post__author">Submitted by {foundedUser.username}</div>
+                {LoggedInUserButtons()}
+            </section>
+        }
+        return RenderPosts()
           })}
       </div>
     </>
