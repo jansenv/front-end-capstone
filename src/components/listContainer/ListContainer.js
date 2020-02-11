5923888dcd99776e32b21caa54edf4203b2ce826
Jansen <jansenv44@gmail.com>
2020-02-11 12:01:13 -0600

    POSTS CAN FILTER NOW FUCK YEAH

--- src/components/listContainer/ListContainer.js
+++ src/components/listContainer/ListContainer.js
@@ -10,7 +10,7 @@ import Button from '@material-ui/core/Button';
 
 
 export default (props) => {
-  const [postsArray, setPosts] = useState({})
+  const [postsObject, setPosts] = useState({})
   const { posts, deletePost } = useContext(PostContext)
   const { topics } = useContext(TopicContext)
   const { users } = useContext(UserContext)
@@ -25,40 +25,76 @@ export default (props) => {
 
   const sortedPostsArray = sortArrayByMostRecent(posts)
 
+  const handleControlledInputChange = (evt) => {
+    /*
+        When changing a state object or array, always create a new one
+        and change state instead of modifying current one
+    */
+    const newPost = Object.assign([], postsObject)
+    newPost[evt.target.name] = evt.target.value
+    console.log(newPost)
+    setPosts(newPost)
+  }
+
+  const filteredPostsByTopic = posts.filter(post => post.topicId === parseInt(postsObject.topicId)) || []
+  // array vs. state
+
+  console.log(filteredPostsByTopic)
+
 
 
   return (
 
     <>
 
-    <main id="main">
-      <div className="sidebar">
-        <h2>Directory</h2>
-        {
-          topics.map(topic => {
-            return <button color="primary" variant="contained" key={topic.id} value={topic.id} onClick={(event) => {
-              console.log("value of button", event.target.value)
-            }}>
-              {topic.name}
-            </button>
-          })}
-      </div>
+      <main id="main">
+        <div className="sidebar">
+          <h2>Directory</h2>
+          {
+            topics.map(topic => {
+              return <button color="primary" variant="contained" key={topic.id} name="topicId" value={topic.id} onClick={handleControlledInputChange}>
+                {topic.name}
+              </button>
+            })}
+          {/* <select
 
-      <div className="posts">
-        <h2 className="PostsHeader">All posts</h2>
+            value={postsObject.topicId}
+            name="topicId"
 
-        <Button id="CreatePostButton" color="secondary" variant="contained" onClick={() => props.history.push("/create")}>
-          Submit a Post
+            id="topicId"
+            className="form-control"
+            onChange={handleControlledInputChange}
+
+          >
+
+            <option value="0">Select Topic</option>
+            {topics.map(topic => (
+              <option key={topic.id} value={topic.id}>
+                {topic.name}
+              </option>
+
+
+            ))}
+          </select> */}
+        </div>
+
+        <div className="posts">
+          <h2 className="PostsHeader">All posts</h2>
+
+          <Button id="CreatePostButton" color="secondary" variant="contained" onClick={() => props.history.push("/create")}>
+            Submit a Post
         </Button>
 
-        {
-          sortedPostsArray.map(post => {
+          {}
+
+          {
+          filteredPostsByTopic.map(post => {
             const foundedUser = users.find(u => u.id === post.userId) || {}
 
             return < Post key={post.id} post={post} user={foundedUser} {...props} />
           })}
-      </div>
-    </main>
+        </div>
+      </main>
 
     </>
   )
