3e15fde3e0518c1017cb7af0595c962f589d3458
Jansen <jansenv44@gmail.com>
2020-02-11 07:33:51 -0600

    styling sign out button

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -16,42 +16,41 @@ import { ThemeProvider, Button } from "@material-ui/core"
 export default (props) => {
     return (
         <>
-        <ThemeProvider theme={theme}>
-            <CommentProvider>
-                <TopicProvider>
-                    <UserProvider>
-                        <PostProvider>
-                            {
-                                localStorage.getItem("activeUser")
-                                    ? <p className="logout__text">
-                                        <Button color="error" variant="contained" className="logout__link"
-                                            to=""
-                                            component={Link}
-                                            onClick={e => {
-                                                e.preventDefault()
-                                                localStorage.removeItem("activeUser")
-                                                props.history.push("/")
-                                            }}>Sign Out
-                                        </Button></p> : ""}
-                            <Route exact path="/"
-                                render={props => <ListContainer {...props} />}
-                            />
-                            <Route path="/create"
-                                render={props => <PostForm {...props} />}
-                            />
-                            <Route path="/edit/:postId(\d+)"
-                                render={props => <PostForm {...props} />}
-                            />
-                            <Route path="/:postId(\d+)"
-                                render={props => <PostDetails {...props} />}
-                            />
-                            <Route exact path="/:postId(\d)+"
-                                render={props => <CommentForm {...props} />}
-                            />
-                        </PostProvider>
-                    </UserProvider>
-                </TopicProvider>
-            </CommentProvider>
+            <ThemeProvider theme={theme}>
+                <CommentProvider>
+                    <TopicProvider>
+                        <UserProvider>
+                            <PostProvider>
+                                {
+                                    localStorage.getItem("activeUser")
+                                        ? <Button id="logOutButton" variant="contained" className="logout__link"
+                                                to=""
+                                                component={Link}
+                                                onClick={e => {
+                                                    e.preventDefault()
+                                                    localStorage.removeItem("activeUser")
+                                                    props.history.push("/")
+                                                }}>Sign Out
+                                        </Button> : ""}
+                                <Route exact path="/"
+                                    render={props => <ListContainer {...props} />}
+                                />
+                                <Route path="/create"
+                                    render={props => <PostForm {...props} />}
+                                />
+                                <Route path="/edit/:postId(\d+)"
+                                    render={props => <PostForm {...props} />}
+                                />
+                                <Route path="/:postId(\d+)"
+                                    render={props => <PostDetails {...props} />}
+                                />
+                                <Route exact path="/:postId(\d)+"
+                                    render={props => <CommentForm {...props} />}
+                                />
+                            </PostProvider>
+                        </UserProvider>
+                    </TopicProvider>
+                </CommentProvider>
             </ThemeProvider>
         </>
     )
